e4c0b3f8351f6956ba0b80786310ab07458d0d5e
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2023-04-15 19:28:02 +0530

    debugging

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write(
-      "<head><title>Enter Message</title><meta charset='UTF-8'></head>"
-    );
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message' ><button type='submit'>Submit</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-    });
+const server = http.createServer(routes);
 
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  res.setHeader("Content-type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first Page</title></head>");
-  res.write("<body><p>Amrutha Varshini</p></body>");
-  res.write("</html>");
-  res.end();
-});
-
-server.listen(3000);
+server.listen(3001);
