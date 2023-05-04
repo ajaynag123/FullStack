e3633e7b426defb2b775eb51fd721b73c85bd027
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2023-05-04 07:31:14 +0530

    mvc

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    const p = path.join(path.dirname())
+  }
+
+  static fetchAll() {
+    return products;
+  }
+}
