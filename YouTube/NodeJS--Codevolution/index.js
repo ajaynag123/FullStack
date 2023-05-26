9b09efb457bb7b6b6560d40d6221591aa7be25b2
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2023-05-26 08:39:16 +0530

    http

--- YouTube/NodeJS--Codevolution/index.js
+++ YouTube/NodeJS--Codevolution/index.js
@@ -1,16 +1,14 @@
-// 
+const http = require('node:http');
 
-const fs = require('node:fs/promises');
-
-//fs.readFile('./abc.txt', 'utf-8').then((data) => console.log(data)).catch(err => console.log(err));
-
-async function readFile() {
-  try {
-    const data = await fs.readFile('./file.txt', 'utf-8');
-    console.log(data);
-  } catch (err) {
-    console.log(err);
+const server = http.createServer((req, res) => {
+  const superHero = {
+    'name': 'Ajay',
+    'last': 'Nag'
   }
-}
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+  res.end(JSON.stringify(superHero));
+})
 
-readFile();
+server.listen(3000, () => {
+  console.log('Server listening');
+})
