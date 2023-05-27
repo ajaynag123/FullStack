6aae0da04cab38068e1fece41ccee0582e3816ad
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2023-05-27 09:31:25 +0530

    async

--- YouTube/NodeJS--Codevolution/index.js
+++ YouTube/NodeJS--Codevolution/index.js
@@ -1,12 +1,22 @@
 const http = require('node:http');
+const fs = require('node:fs')
 
 const server = http.createServer((req, res) => {
-  const superHero = {
-    'name': 'Ajay',
-    'last': 'Nag'
+  const person = { name: 'ajay', age: 45 }
+  if (req.url === '/') {
+    res.writeHead(200, { "Content-type": 'text/plain' });
+    res.end('Home page')
   }
-  res.writeHead(200, { 'Content-type': 'text/plain' });
-  res.end(JSON.stringify(superHero));
+  else if (req.url === '/about') {
+    res.writeHead(200, { "Content-type": 'text/plain' });
+    res.end('About page')
+  } else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end(JSON.stringify(person))
+  } else {
+    res.writeHead(404)
+    res.end('Page not found');
+  }
 })
 
 server.listen(3000, () => {
